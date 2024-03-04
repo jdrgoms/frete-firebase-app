2b62acb735813c3329bf58719adb3c40b18bba8b
Jader <jjadergomes@gmail.com>
2024-03-04 19:51:54 -0300

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frete-firebase-app',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
